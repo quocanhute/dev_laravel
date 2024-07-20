25e2a932a1cf3c6ac938cc47e5fddf9a5d4a8813
QuocAnh <quocanhute@gmail.com>
2024-07-20 15:27:40 +0700

    test

---
+++ src/app/Http/Resources/UserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+  public static $wrap = false;
+
+  /**
+   * Transform the resource into an array.
+   *
+   * @param  Request  $request
+   * @return array
+   */
+  public function toArray($request): array
+  {
+    return [
+      'id' => $this->id,
+      'name' => $this->name,
+      'email' => $this->email,
+      'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+    ];
+  }
+}
